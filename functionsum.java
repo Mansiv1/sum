06697a8be5427e7d0730984287bf7daa22b27f53
Mansiv1 <123632934+Mansiv1@users.noreply.github.com>
2023-01-26 16:15:01 +0530

    Add files via upload

---
+++ functionsum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class functionsum {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sum(3, 5));
+        System.out.println(sum(100, 56));
+    }
+}
