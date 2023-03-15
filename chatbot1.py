208bdff916b0e88d1b2df6b91297f71bb43c51a2
Mansiv1 <123632934+Mansiv1@users.noreply.github.com>
2023-03-15 20:38:04 +0530

    Add files via upload
    chatbot

---
+++ chatbot1.py
@@ -0,0 +1,16 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/Verma Group/Desktop/chatbot.py ======
+what is your name?
+mansi verma
+Hi mansi verma, I am Chat Bot. Will you help me learn about your planet ?
+no
+ok, have a Nice Day!
+>>> 
+======= RESTART: C:/Users/Verma Group/Desktop/chatbot.py ======
+what is your name?
+gunjan
+Hi gunjan, I am Chat Bot. Will you help me learn about your planet ?
+no
+ok, have a Nice Day!
